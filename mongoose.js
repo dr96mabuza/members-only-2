22c2a920c57a9e92e42cf27def5a6b9a42700f3d
sifiso mabuza <simabuz022@student.wethinkcode.co.za>
2023-08-14 15:28:47 +0200

    created env file

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,9 @@
-// connect to database
+require('dotenv').config();
 const mongoose = require("mongoose");
+
+// connect to database
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://admin:2JpQfjIXywT7yXkJ@cluster0.fvsxm3m.mongodb.net/?retryWrites=true&w=majority"
+const mongoDB = `mongodb+srv://${process.env.MONGODB_USER }:${process.env.MONGODB_PASSWORD }@cluster0.fvsxm3m.mongodb.net/?retryWrites=true&w=majority`;
 main().catch(err => console.log(err))
 async function main() {
   await mongoose.connect(mongoDB)
