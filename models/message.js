3b6bfa3b4dc2961b7098a5bab25125c884f2cc36
Sifiso <dr96mabuza@gmail.com>
2023-08-22 18:09:56 +0200

    fix brackets

--- models/message.js
+++ models/message.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const messageSchema = new Schema({
-    {user: {type: Schema.Types.ObjectId, required: true}},
-    {timestamp: {type: Date, default: new Date()}},
-    {message: {type: String, required: true}},
+    user: {type: Schema.Types.ObjectId, required: true},
+    timestamp: {type: Date, default: Date.now},
+    message: {type: String, required: true},
 });
 
 messageSchema.virtual("url").get(() => {
