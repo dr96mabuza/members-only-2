1e781a570ea55606209b52c5368dda7b45281ae9
Sifiso <dr96mabuza@gmail.com>
2023-08-23 18:19:46 +0200

    added validator

--- models/message.js
+++ models/message.js
@@ -3,10 +3,10 @@ const user = require("./user");
 
 const Schema = mongoose.Schema;
 const messageSchema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: user, required: true},
-    timestamp: {type: Date, default: Date.now},
-    title: {type: String, required: true, minLength: 2},
-    message: {type: String, required: true},
+    user: { type: Schema.Types.ObjectId, ref: user, required: true },
+    timestamp: { type: Date, default: Date.now },
+    title: { type: String, required: true, minLength: 2 },
+    message: { type: String, required: true },
 });
 
 messageSchema.virtual("url").get(() => {
