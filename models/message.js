c711ab3ea9afb8419eedea2781e8d3feef58704b
Sifiso <dr96mabuza@gmail.com>
2023-08-23 17:13:52 +0200

    added message title

--- models/message.js
+++ models/message.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 const messageSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: user, required: true},
     timestamp: {type: Date, default: Date.now},
+    title: {type: String, required: true, minLength: 2},
     message: {type: String, required: true},
 });
 
